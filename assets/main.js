f2cdc9e2c9947021e47eb515ffd64e9a36acad47
alexismoreyra <alexismoreyra@hotmail.com>
2024-03-25 21:01:19 -0300

    Mi primer repositorio

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+
+var numero1 = 10;
+var numero2 = 10;
+if(numero1 == numero2) {
+    alert ('Son iguales');
+} else {
+    alert('No son iguales');
+}
+
+console.log('Se cargo correctamente el archivo JS');
+
